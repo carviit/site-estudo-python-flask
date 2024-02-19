1ee0f396c92b0f62885a16b44ebfbf98ee86d75c
carviit <carlosfdm@outlook.com>
2024-02-18 23:35:41 -0300

    commiy script banco de dados

---
+++ banco_de_dados.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA site_teste;
+
+USE site_teste;
+
+CREATE TABLE USUARIOS(
+
+id INT NOT NULL AUTO_INCREMENT.
+nome varchar(30) NOT NULL,
+senha varchar(10) NOT NULL,
+PRIMARY KEY(id)
+);
